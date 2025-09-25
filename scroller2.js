280cfc1d1312821176579fca9c9e167eaf41a8ed
Derek Chai <chderek9@gmail.com>
2025-09-26 11:14:34 +1200

    Add third placeholder photograph

--- scroller2.js
+++ scroller2.js
@@ -5,11 +5,13 @@ scroller2.setup({
     })
     .onStepEnter((response) => {
         switch (response.index) {
-            case 0: 
-                hide('figure-enemy2');
-                break;
             case 1:
+                hide('figure-enemy3')
                 show('figure-enemy2')
+                break;
+            case 2:
+                show('figure-enemy2')
+                show('figure-enemy3')
                 break;
             default:
                 hide('figure-enemy2')
